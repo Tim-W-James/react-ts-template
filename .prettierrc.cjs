9054a5175e5eb2714d2c73034063cd26ddd1d517
Tim W James <38764646+Tim-W-James@users.noreply.github.com>
2022-09-19 11:56:31 +1000

    feat: add prettier rules

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,4 +2,9 @@ module.exports = {
   semi: true,
   singleQuote: false,
   trailingComma: "es5",
+  arrowParens: "avoid",
+  printWidth: 80,
+  // ! Where possible, define in ./editorconfig
+  // tabWidth: 2,
+  // useTabs: false,
 };
