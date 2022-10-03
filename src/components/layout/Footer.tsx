3bbbbc948ca55c148ce630055cdbc86e9cd12b07
Tim W James <38764646+Tim-W-James@users.noreply.github.com>
2022-10-03 19:32:21 +1100

    feat: update footer by date

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,9 @@
 const Footer: React.FC = () => (
   <footer className="bg-dark" id="footer">
     <div className="container">
-      <span className="text-muted">© 2022 Company, Inc</span>
+      <span className="text-muted">
+        © {new Date().getFullYear()} Company, Inc
+      </span>
     </div>
   </footer>
 );
